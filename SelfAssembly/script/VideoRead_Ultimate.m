ca5fb31f3898c1f5a5b18fc07f0e8630635c0a20
Shaw Haines <haohaistone@qq.com>
2021-01-25 16:42:58 +0800

    Adjusted the VideoRead_Ultimate.

--- SelfAssembly/script/VideoRead_Ultimate.m
+++ SelfAssembly/script/VideoRead_Ultimate.m
@@ -1,8 +1,7 @@
 %% Preparations and Definitions
 close all;
 % You can adjust it manually.
-uniformSize=1050;
-FrameSize=25;
+uniformSize=1050; % somewhat legacy... Just don't touch it...
 
 oldPath=pwd;
 cd(folder);
@@ -15,9 +14,9 @@ frame=imcrop(readFrame(vid1),[0,0,1920,1080]);%crop the 8 excessive pixels off.
 
 bw=imbinarize(rgb2gray(frame));
 [centers, radii, metric] = imfindcircles(bw,[450 540],'Sensitivity',0.99,'ObjectPolarity','dark');
-figure;
-imshow(bw);
-viscircles(centers, radii,'EdgeColor','b');
+% figure;
+% imshow(bw);
+% viscircles(centers, radii,'EdgeColor','b');
 
 %rect[xmin ymin width height] y is calculated from above
 rect=zeros(1,4);
@@ -27,22 +26,14 @@ rect(2)=centers(1,2)-rect(3)/2;
 rect(1)=centers(1,1)-rect(3)/2;
 
 %% Output Frames
-if vid1.Duration<2.5
-    vid1.CurrentTime=0;
-else
-    vid1.CurrentTime=vid1.Duration-2.5;%the last 2.5 seconds. Avoid shaking in the last.
-end
-
 i=1;
-while hasFrame(vid1)&& i<=FrameSize*2
+while hasFrame(vid1)
     frame=readFrame(vid1);
     %imshow(frame);
-    if mod(i,2)==0
-        cropped=imresize(imcrop(frame,rect),[uniformSize,uniformSize]);
-        imwrite(cropped,i/2+".jpg");
-    end
+    cropped=imresize(imcrop(frame,rect),[uniformSize,uniformSize]);
+    imwrite(cropped,i+".jpg");
     i=i+1;
 end
 
 cd(oldPath);
-fprintf("video extracted in %s\\%s.\n",folder,baseName)
+fprintf("video extracted in %s\\%s.\n",folder,baseName);
