5885328c17c6bab28cc6ff6fe6eb6d83f27345d4
Shaw Haines <haohaistone@qq.com>
2021-01-25 11:25:32 +0800

    Iterative version fft, not successful.

--- script/myFFT.m
+++ script/myFFT.m
@@ -1,21 +1,34 @@
 function y=myFFT(x)
 N=length(x);
 m=log2(N);
-if N==1
-    y=[x];
-    return;
-end
 if ceil(m)~=m % not an exponent of 2, add 0 at the tail
     m=ceil(m);
     N=2^m;
     x(N)=0; % A good method of resizing and adding 0s.
 end
-indices=2:2:N;
-yo=myFFT(x(indices)); % odd, note that this is because the index starts from 0
-ye=myFFT(x(indices-1)); % even
-y=zeros([1,N]);
-omega=exp((0:N/2-1)*(-1i)*2*pi/N);
-indices=1:N/2;
-y(indices)=ye+omega.*yo;
-y(indices+N/2)=ye-omega.*yo;
+A1=x;
+A2=zeros([1,N]);
+omega=exp((0:(N/2-1))*-1i*2*pi/N);
+for l=1:m
+    if rem(l,2)~=0
+        for k=0:(2^(m-l)-1)
+            for j=0:(2^(l-1)-1)
+                A2(k*2^l+j+1)=A1(k*2^(l-1)+j+1)+A1(k*2^(l-1)+j+1+N/2);
+                A2(k*2^l+j+1+2^(l-1))=A1(k*2^(l-1)+j+1)-A1(k*2^(l-1)+j+1+N/2)*omega((k*2^(l-1))+1);
+            end
+        end
+    else
+        for k=0:(2^(m-l)-1)
+            for j=0:(2^(l-1)-1)
+                A1(k*2^l+j+1)=A2(k*2^(l-1)+j+1)+A2(k*2^(l-1)+j+1+N/2);
+                A1(k*2^l+j+1+2^(l-1))=A2(k*2^(l-1)+j+1)-A2(k*2^(l-1)+j+1+N/2)*omega((k*2^(l-1))+1);
+            end
+        end
+    end
+end
+if rem(m,2)==0
+    y=A2;
+else
+    y=A1;
+end
 end
