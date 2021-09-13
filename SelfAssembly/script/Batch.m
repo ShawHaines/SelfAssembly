94291ad76e5a733e4917250baff4701543566469
Shaw Haines <shawhaines@zju.edu.cn>
2021-09-13 21:41:45 +0800

    Rewrote `OrderParameter.m` based on `localOrientationOrder.m`

--- SelfAssembly/script/Batch.m
+++ SelfAssembly/script/Batch.m
@@ -1,7 +1,7 @@
 % Preparations
 clearvars;
 close all;
-folder="E:/Video Recordings/ball/aluminium/aluminium sweep_filling 55Hz 1.40/0724";
+folder="E:/Video Recordings/ball/aluminium/aluminium sweep_filling 55Hz 1.40/0725";
 list=dir(folder+"/*.MOV");
 %% Batch Loop
 for loop=1:length(list) % this loop name is to avoid name conflicts with VideoRead etc.
@@ -10,7 +10,7 @@ for loop=1:length(list) % this loop name is to avoid name conflicts with VideoRe
     extName=temp(end);
     baseName=erase(fileName,"."+extName);
     try
-        localOrientationOrder(folder+"/"+baseName+"/"+baseName+"_tracing.mat",folder+"/"+baseName+"/",baseName);
+        orderParameter(folder+"/"+baseName+"/"+baseName+"_tracing.mat",folder+"/"+baseName+"/",baseName);
     catch ME
         close all;
         continue;
--- SelfAssembly/script/OrderParameter.m
+++ SelfAssembly/script/OrderParameter.m
@@ -1,71 +1,84 @@
-%数据处理程序——序参量：
-for i1=1:a
-    if (radii(i1)<10) && radii(i1)>0&& ((centers(i1,1)-shuiping/2)^2+(centers(i1,2)-shuzhi/2)^2)<380^2
-        if j2==1 
-           center(j2,1)=centers(i1,1);
-           center(j2,2)=centers(i1,2); 
-           j2=j2+1;
-        end
-        if j2>1
-           for k=1:j2-1
-               if ((centers(i1,1)-center(k,1))^2+( centers(i1,2)-center(k,2))^2<5^2)
-                   k=k-1;
-                  break
-               end
-           end
-            if k==j2-1
-               center(j2,1)=centers(i1,1);
-               center(j2,2)=centers(i1,2);
-               radi(j2)=radii(i1);
-               j2=j2+1;
-            end
-        end
+% orderParameter.m
+% Based on the tracing data `.mat`, output a video or figure that visualize
+% the kernels that form a hexagonal close pack. The parameters are like the
+% ones in localOrientationOrder.m
+function lastFrameIndex=orderParameter(dataPath,savePath,baseName)
+%% Preparation and parameter setup
+% close all
+
+% the diameter of particles, unit in mm.
+% Aluminium 19.54 +- 0.30*3, Copper 19.38 +- 0.18*3, Iron 19.38 +- 0.23*3
+D=19.38;
+% the interparticle distance when ordered.
+dD=0.036*D;% ratio: Aluminium 0.046, iron 0.036, copper 0.027
+
+% if true, it will plot the granules and the image to assist revision
+ACTIVATEPLOT=true;
+tic;
+saveBase=sprintf("%s/%s_orderParameter",savePath,baseName);
+% if exist('ACTIVATEPLOT','var')~=0 && ACTIVATEPLOT
+%     figure;
+%     writer=VideoWriter(char(saveBase+".avi")); % Strange that it does not seem to support string.
+%     writer.FrameRate=25.0;
+%     open(writer);
+% end
+%% load data
+load(dataPath);
+% sampleSize=size(center,1);
+sampleSize=1;
+sourceCount=size(center{1},1);
+sidelength=1050/2;
+
+centerArray=zeros([sampleSize,sourceCount,2]);
+velocity=zeros([sampleSize-1,sourceCount,2]);
+centerArray(1,:,:)=center{1};
+% FIXME: There might be NaNs, be careful about how to deal with them.
+for i=2:sampleSize
+    centerArray(i,:,:)=center{i};
+    velocity(i-1,:,:)=centerArray(i,:,:)-centerArray(i-1,:,:); % unit in pixels.
+end
+%% visualization
+% orderList=zeros([sampleSize,sourceCount]); % the center and the vicinity of a hexagon
+% kernelList=zeros([sampleSize,sourceCount]);% the center of a hexagon
+rBarList=zeros([sampleSize,sourceCount]); % the mean bond length of a vertex.
+for frameIndex=1:sampleSize
+    % orientation order
+    DT=delaunayTriangulation(centerArray(frameIndex,:,1)',centerArray(frameIndex,:,2)');
+    % accept id as a column vector.
+    meanBondLengthFunction=@(id) mean(vecnorm(bonds(DT,id),2,2),'omitnan');
+    rBar=arrayfun(meanBondLengthFunction,1:size(DT.Points,1));
+    rBarList(frameIndex,:)=rBar;
+    
+    if ACTIVATEPLOT
+        figure;
+        hold off;
+        scatter(centerArray(frameIndex,:,1),centerArray(frameIndex,:,2),16,abs(rBar),'filled');
+        hold on;
+        viscircles([sidelength+0.5,sidelength+0.5],sidelength);
+        caxis([20,40]);
+        bar=colorbar;
+        colormap jet;
+        bar.Label.Interpreter="latex";
+        bar.Label.String="$\bar{r}$";
+        axis ij;
+        axis equal;
+        savefig(gcf,saveBase+".fig",'compact');
+%         drawnow;
+%         writeVideo(writer,getframe(gcf));
+    else
+        frameIndex % display the progress...
     end
 end
-[a,b]=size(radi);
-TRI=delaunay(center(:,1),center(:,2));
-[a1,b1]=size(TRI);
-n=zeros(b,12);
-for j2=1:a1
-    for k=1:3
-        if n(TRI(j2,k),1)==0
-           for m=1:3
-               if m~=k
-                  n(TRI(j2,k),1)=n(TRI(j2,k),1)+1;
-                  n(TRI(j2,k),n(TRI(j2,k),1)+1)=TRI(j2,m);
-               end
-           end
-        end
-        if n(TRI(j2,k),1)~=0
-            for m=1:3
-                if m~=k
-                    for p=2:n(TRI(j2,k),1)+1
-                        if TRI(j2,m)==n(TRI(j2,k),p)
-                            p=p-1;
-                            break
-                        end
-                    end
-                if p==n(TRI(j2,k),1)+1
-                       n(TRI(j2,k),1)=n(TRI(j2,k),1)+1;
-                       n(TRI(j2,k),n(TRI(j2,k),1)+1)=TRI(j2,m);
-                    end
-                end
-            end
-        end
-    end
+%%
+% if ACTIVATEPLOT
+%     close(writer);
+%     fprintf("Saved data in %s.avi.\n",saveBase);
+% end
+lastFrameIndex=frameIndex;
+% there might be empty cells in the tail of the cell array.
+rBarList=rBarList(1:lastFrameIndex,:);
+% save(saveBase+".mat",'rBarList');
+% fprintf("Saved data in %s.mat\n",saveBase);
+fprintf("finished.\n");
+toc
 end
-theta=zeros(b,12);
-q=6;op1(i)=0;
-for i1=1:b
-    j2=n(i1,1);
-    for k=2:j2+1
-        theta(i1,k)=atan2((center(n(i1,k),2)-center(i1,2)),(center(n(i1,k),1)-center(i1,1)));
-theta(i1,1)=exp(1j*q*theta(i1,k))+theta(i1,1);
-    end
-    theta(i1,1)=theta(i1,1)/j2;
-    op1(i)=op1(i)+abs(theta(i1,1));
-end
-op1(i)=op1(i)/b;
-op=op+op1(i);
-end
-op=op/25
