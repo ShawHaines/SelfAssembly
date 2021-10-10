a721a6a97da6af18f9bd5b98fb83144a22c2b53e
Shaw Haines <shawhaines@zju.edu.cn>
2021-10-10 19:21:21 +0800

    Added Translational order parameter.

---
+++ SelfAssembly/script/myPlot.m
@@ -0,0 +1,10 @@
+function [I]=myPlot(ImSize,positions)
+% plot the points of certain (x,y) coordinates on Image of given size (ImSize).
+% require positions to be of size n*2 list, ImSize 1*2.
+I=zeros(ImSize);
+rounded=round(positions);
+for index=1:size(positions,1) % ugly grammar, can't use array index
+    I(rounded(index,1),rounded(index,2))=1;
+end
+fprintf("total %d particles",size(positions,1));
+end
