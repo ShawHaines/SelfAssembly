0115c9edaa3d250d0797d61a315d253510e88350
Shaw Haines <shawhaines@zju.edu.cn>
2021-10-11 22:49:15 +0800

    Reduced `basin.m` to a pure function that finds the basin.

--- SelfAssembly/script/basin.m
+++ SelfAssembly/script/basin.m
@@ -1,35 +1,20 @@
-frameIndex=1;
-DT=delaunayTriangulation(centerArray(frameIndex,:,1)',centerArray(frameIndex,:,2)');
+% idx=basin(x,y,vertexValue,allowance)
+% Find the largest basin (that is, the change of `vertexValue` on an edge is
+% smaller than allowance) of a given point set (x,y).
+function idx=basin(x,y,vertexValue,allowance)
+DT=delaunayTriangulation(x,y);
 edgeList=DT.edges;
 weightFun=@(a,b) abs(vertexValue(a)-vertexValue(b));
 edgeWeight=arrayfun(weightFun,edgeList(:,1),edgeList(:,2));
 
-%% You have to plot the edges yourself...
-% And you even have to write a colormap mapping function ...
-colormap('jet');
-map=colormap;
-colorRange=[0,1];
-figure;
-edgePlot(DT.Points,edgeList,edgeWeight,map,colorRange);
-axis equal;
-caxis(colorRange);
-colorbar;
-title("Original");
 %% find the basin in the center, filter out the edges where abrupt change occur.
-allowance=1;
+% allowance=1;
 edgeList=edgeList(edgeWeight<allowance,:);
-edgeWeight=edgeWeight(edgeWeight<allowance);
-
-figure;
-edgePlot(DT.Points,edgeList,edgeWeight,map,colorRange);
-axis equal;
-caxis(colorRange);
-colorbar;
-title("After cutting off abrupt changes");
+% edgeWeight=edgeWeight(edgeWeight<allowance);
 
 % find the largest connected component
 G=graph(edgeList(:,1),edgeList(:,2));
 % plot(G); % Hmm, interesting...
 [bins,binSize]=conncomp(G);
 idx=find(binSize(bins)==max(binSize)); % the indices of vertices that should remain.
-
+end
